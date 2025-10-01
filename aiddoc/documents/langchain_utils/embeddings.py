a4c42c316cfe1b8ef460eff504fc0c2621c49a71
prakkhar03 <prakkhar.n03@gmail.com>
2025-10-01 21:44:12 +0530

    Added Fallback

--- aiddoc/documents/langchain_utils/embeddings.py
+++ aiddoc/documents/langchain_utils/embeddings.py
@@ -4,4 +4,12 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 
 def get_embeddings():
     
-    return HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+    # return HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+    try:
+        return GoogleGenerativeAIEmbeddings(
+        model_name="gemini-pro",
+        api_key=os.getenv("GOOGLE_API_KEY")
+    )
+    except Exception as e:
+        print(f"Error initializing GoogleGenerativeAIEmbeddings: {e}")
+        return HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
